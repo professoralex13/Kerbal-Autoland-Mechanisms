3d0e4b5d5442bf7594e76948db0cfd4115474942
professoralex13 <alex@densem.net>
2020-11-15 20:23:01 +1300

    Updated for Visual Studio

--- Kerbal Autoland Mechanisms/Properties/AssemblyInfo.cs
+++ Kerbal Autoland Mechanisms/Properties/AssemblyInfo.cs
@@ -1,34 +1,35 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kerbal_Autoland_Mechanisms")]
+[assembly: AssemblyTitle("Kerbal Autoland Mechanisms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Kerbal_Autoland_Mechanisms")]
+[assembly: AssemblyProduct("Kerbal Autoland Mechanisms")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("C35C9A47-BB62-4373-8708-496CBF1D207C")]
+[assembly: Guid("9ce50175-c8a5-4ea7-a1df-57cb0fd3b3ce")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
